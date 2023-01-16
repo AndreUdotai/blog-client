558e777f985d3dcab0992fdef4c5bc3256051dc1
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-01-16 21:55:17 +0100

    added comments to the js codes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
-// let postTitle = document.getElementById('post-title');
 let postList = document.getElementById('post-list');
 
+// Fetch all the posts from blog-api
 fetch('http://localhost:3000/api/blog/posts')
     .then((response) => response.json())
     .then((data) => {
+        // Inititialize an empty array
         let posts = [];
+        // Loop over the posts json data returned from blog-api
         for (let element of data) {
+            // Push all the posts returned from blog-api in posts array
             posts.push(element);
         }
+        // Reverse the order of the posts in the array to make the latest
+        // post appears first
         posts.reverse();
+        // Loop over the posts in the posts array
         for (let post of posts) {
+            // Create a new div element
             let postElement = document.createElement('div');
-            postElement.innerHTML = `
+            // Set the posts-display html below as the inner element of
+            // the newly created div element
+
+            let postListComponent = `
             <div class='post-preview'>
                 <a href='#'id='post-title'>
                     <h2 class='post-title' data-id='${post._id}' >
@@ -26,14 +36,19 @@ fetch('http://localhost:3000/api/blog/posts')
             </div>
             <hr class="my-4" />
             `;
+            postElement.innerHTML = postListComponent;
+            // Append the posts component on the postList element
             postList.appendChild(postElement);
         }
     });
 
+// Set a click event listener on the title of posts to extract the
+// id of the post and save in the postId variable
 postList.addEventListener('click', (e) => {
-    console.log(e.target.dataset.id)
-    postId = e.target.dataset.id;
+    let postId = e.target.dataset.id;
 
+    // Use the extracted id saved in postId to fetch the full detail of 
+    // the post from the database
     fetch('http://localhost:3000/api/blog/post/' + postId)
         .then((response) => response.json())
         .then((data) => console.log(data));
